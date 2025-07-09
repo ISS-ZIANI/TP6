3dc34f1b29760748f370d7d0c6992dc576af20f7
TonNom <tonemail@example.com>
2025-07-09 03:09:05 +0200

    Initial commit - whole project

---
+++ mcp-client/src/main/java/net/youssfi/mcpclient/controllers/AIRestController.java
@@ -0,0 +1,19 @@
+package net.youssfi.mcpclient.controllers;
+
+import net.youssfi.mcpclient.agents.AIAgent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AIRestController {
+    private AIAgent agent;
+
+    public AIRestController(AIAgent agent) {
+        this.agent = agent;
+    }
+    @GetMapping("/chat")
+    public String chat(String query){
+        return agent.askLLM(query);
+    }
+
+}
---
+++ mcp-server/src/main/java/net/youssfi/mcpserver/McpServerApplication.java
@@ -0,0 +1,23 @@
+package net.youssfi.mcpserver;
+
+import net.youssfi.mcpserver.tools.StockTolls;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class McpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerApplication.class, args);
+    }
+
+    @Bean
+    public MethodToolCallbackProvider getMethodToolCallbackProvider() {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(new StockTolls())
+                .build();
+    }
+
+}
